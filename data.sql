45dae88dfb21b9c3879f80747f105a9e09998abd
Stephanie <smmackin12@gmail.com>
2022-08-15 16:29:05 -0400

    bookstore

---
+++ data.sql
@@ -0,0 +1,13 @@
+\c books 
+\c books_test
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
